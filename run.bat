082d5963f51fa45216b5e5aed5f12477c24bf113
Exortions <75327059+Exortions@users.noreply.github.com>
2022-06-02 12:28:19 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,10 @@
 @echo off
 
 build.exe
+python minify.py build/creator.py
 echo Copy the file build/creator.py to your robot
 echo Run the program, and place the output in config/input.json
 pause
 compile.exe
+python minify.py build/autonomous.py
 echo Finished! Run the build/autonomous.py on your robot.
